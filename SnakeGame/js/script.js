266dae18fcc8c9c3d7ed0f89c945e143fd2addc3
ssdsldd <noryaivanov@gmail.com>
2022-11-13 23:58:12 +0600

    add some function

--- SnakeGame/js/script.js
+++ SnakeGame/js/script.js
@@ -6,6 +6,14 @@ let width = canvas.width;
 let height = canvas.height;
 let score = 0;
 let blockCell = 10;
+let widthInBlock = width/blockCell;
+let heightInBlock = height/blockCell;
+
+
+function Block(col, row){
+    this.col = col;
+    this.row = row;
+}
 
 function circle(x, y, r, fillCircle, colorCircle){
     ctx.beginPath();
@@ -18,5 +26,26 @@ function circle(x, y, r, fillCircle, colorCircle){
     }
 }
 
+function drawBorder(){
+    ctx.fillStyle = "Gray";
+    ctx.fillRect(0, 0, width, blockCell);
+    ctx.fillRect(0, height-blockCell, width, blockCell);
+    ctx.fillRect(0, 0, blockCell, height);
+    ctx.fillRect(width-blockCell, 0, blockCell, height);
+}
 
+function drawScore(){
+    ctx.font = "20px Courier";
+    ctx.textBaseline = "top";
+    ctx.textAlign = "left";
+    ctx.fillStyle = "black"
+    ctx.fillText("Score:" + score, blockCell, blockCell);
+}
 
+function gameOver(){
+    clearInterval(intervalID);
+    ctx.font = "60px Courier";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "black";
+    ctx.fillText("Game Over", width/2, height/2);
+}
